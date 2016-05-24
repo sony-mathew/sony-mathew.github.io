b90051c4c2c16a6da865654c72d1956f4a16b474
sony mathew <sony@freshdesk.com>
2016-05-24 20:31:58 +0530

    worker change 24 May 08.31

---
+++ mf_worker.js
@@ -0,0 +1,6 @@
+/* 
+  Version 1.0.1
+*/
+'use strict';
+var WEBSITE_KEY = 'db6c4f9a68611314f08195e7a4da6052ef780ea5874fbe49e823778857b236d1';
+importScripts("https://cdn.marketfox.io/mf_remote_worker.js");
