4f9695cd652f03b645dd69dd8ff798324c2c280d
Sony Mathew <ynos1234@gmail.com>
2017-01-27 14:05:52 -0800

    prod & staging script testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sony-mathew.github.io
 Setting up Github pages for my portfolio.
+
+This is my playground.
---
+++ staging/mf_worker.js
@@ -0,0 +1,6 @@
+/* 
+  Version 1.0.1
+*/
+'use strict';
+var WEBSITE_KEY = '4c8d2576c874868e3cf8c0d79d5605725dd10b407c6e033a212f6f1ab8eb21bf';
+importScripts("https://assets-staging-marketfox-io.s3.amazonaws.com/mf_remote_worker.js");
