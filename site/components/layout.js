d963e67e5897928e45fa949202fb2c63155b2228
sony-mathew <ynos1234@gmail.com>
2020-10-23 15:18:44 +0530

    feat: made the website repsonsive
    the major problem was the navigation bar/menu when it comes to mobile. made this responsive.

--- site/components/layout.js
+++ site/components/layout.js
@@ -2,6 +2,133 @@ import Head from "next/head";
 import styles from "./layout.module.scss";
 import Link from "next/link";
 import DEFAULT_CONFIG from '../config/default_config'
+import { useState } from 'react';
+
+function DesktopLayout() {
+  return (
+    <>
+      {/* For Desktop */}
+      <div className="grid grid-cols-6 flex-row mb-8">
+        <div className="col-start-1 col-span-2 flex flex-grow text-gray-700 text-center pl-0">
+          <Link href="/">
+            <a className="text-lg text-gray-600 hover:no-underline py-1">
+              The Usual 🎙
+            </a>
+          </Link>
+        </div>
+        <div className="col-start-3 col-span-2 flex flex-row gap-x-4 text-center">
+          <Link href="/blog">
+            <a className="nav-item text-lg text-gray-600 hover:no-underline m-4 mx-4">Blog</a>
+          </Link>
+          <Link href="/projects">
+            <a className="nav-item text-lg text-gray-600 hover:no-underline m-4 mx-4">
+              Projects
+            </a>
+          </Link>
+        </div>
+        <div className="col-span-1 flex flex-row"></div>
+        <div className="col-span-1 flex flex-row gap-x-4 m-2">
+          <SocialButtons />
+        </div>
+      </div>
+    </>
+  )
+}
+
+function MobileLayout() {
+  const [navMenuOpened, setNavMenuOpened] = useState(false);
+
+  return (
+    <>
+      {/* For Mobile */}
+      <MobileNavMenuSidebar navMenuOpened={navMenuOpened} setNavMenuOpened={setNavMenuOpened} />
+
+      <ul className="inline-flex m-0">
+        <li>
+          <button className="py-2 text-gray-600 appearance-none focus:outline-none" onClick={() => setNavMenuOpened(true)}>
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </li>
+        <li className="mx-4">
+          <Link href="/">
+            <a className="text-lg text-gray-600 hover:no-underline py-1">
+              The Usual 🎙
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function MobileNavMenuSidebar({ navMenuOpened, setNavMenuOpened }) {
+  if (!navMenuOpened) {
+    return (<></>);
+  }
+
+  return (
+    <div className="w-full absolute top-0 left-0 z-10 m-0 bg-gray-700">
+      <div className="absolute top-0 right-0 m-4" onClick={() => setNavMenuOpened(false)}>Close</div>
+      <div className="flex flex-col m-4 gap-4">
+        <Link href="/">
+          <a className="text-lg text-gray-400 hover:no-underline px-2">
+            Home
+          </a>
+        </Link>
+        <Link href="/blog">
+          <a className="text-lg text-gray-400 hover:no-underline px-2">
+            Blog
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className="text-lg text-gray-400 hover:no-underline px-2">
+            Projects
+          </a>
+        </Link>
+        <div className="flex flex-row gap-x-4 px-2">
+          <SocialButtons />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SocialButtons() {
+  return (
+    <>
+      <Link href="https://github.com/sony-mathew">
+        <a target="_blank" className="md:flex-shrink-0">
+          <img
+            src="/icons/github.svg"
+            className={`${styles.icon}`}
+            alt="Github"
+          />
+        </a>
+      </Link>
+      <Link href="https://twitter.com/sonymathew_">
+        <a target="_blank">
+          <img
+            src="/icons/twitter.svg"
+            className={`${styles.icon}`}
+            alt="Twitter"
+          />
+        </a>
+      </Link>
+      <Link href="https://www.linkedin.com/in/sonymathew/">
+        <a target="_blank">
+          <img
+            src="/icons/linkedin.svg"
+            className={`${styles.icon}`}
+            alt="LinkedIn"
+          />
+        </a>
+      </Link>
+    </>
+  );
+}
 
 export default function Layout({ children, home }) {
   return (
@@ -22,54 +149,11 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header>
-        <div className="grid grid-cols-6 flex-row mb-8">
-          <div className="col-start-1 col-span-2 flex flex-grow text-gray-700 text-center pl-0">
-            <Link href="/">
-              <a className="text-lg text-gray-600 hover:no-underline py-1">
-                The Usual 🎙
-              </a>
-            </Link>
-          </div>
-          <div className="col-start-3 col-span-2 flex flex-row gap-x-4 text-center">
-            <Link href="/blog">
-              <a className="nav-item text-lg text-gray-600 hover:no-underline px-4 py-1">Blog</a>
-            </Link>
-            <Link href="/projects">
-              <a className="nav-item text-lg text-gray-600 hover:no-underline px-4 py-1">
-                Projects
-              </a>
-            </Link>
-          </div>
-          <div className="col-span-1 flex flex-row"></div>
-          <div className="col-span-1 flex flex-row gap-x-4 m-2">
-            <Link href="https://github.com/sony-mathew">
-              <a target="_blank">
-                <img
-                  src="/icons/github.svg"
-                  className={`${styles.icon}`}
-                  alt="Github"
-                />
-              </a>
-            </Link>
-            <Link href="https://twitter.com/sonymathew_">
-              <a target="_blank">
-                <img
-                  src="/icons/twitter.svg"
-                  className={`${styles.icon}`}
-                  alt="Twitter"
-                />
-              </a>
-            </Link>
-            <Link href="https://www.linkedin.com/in/sonymathew/">
-              <a target="_blank">
-                <img
-                  src="/icons/linkedin.svg"
-                  className={`${styles.icon}`}
-                  alt="LinkedIn"
-                />
-              </a>
-            </Link>
-          </div>
+        <div className="hidden lg:block">
+          <DesktopLayout />
+        </div>
+        <div className="block lg:hidden">
+          <MobileLayout />
         </div>
       </header>
       <main>{children}</main>
