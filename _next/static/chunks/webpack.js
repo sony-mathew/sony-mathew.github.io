dcf6b33d225afb89f25e244d7b0984753d7b72f4
sony-mathew <ynos1234@gmail.com>
2020-10-24 00:54:50 +0530

    Deploy: 2020-10-24 00:54:50 +0530

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a8bc51f897ab3b2707b";
+/******/ 	var hotCurrentHash = "7fa2162e0ab0e7f33880";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
