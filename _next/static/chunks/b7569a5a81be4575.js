9f1bc7e864d75bb2e3728787fb0f6214614c2587
sony-mathew <ynos1234@gmail.com>
2026-05-31 03:05:56 +0000

    Deploy: 2026-05-31 03:05:56 +0000

--- _next/static/chunks/b7569a5a81be4575.js
+++ _next/static/chunks/b7569a5a81be4575.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/aaaae769ffa866de.js",
   "static/chunks/204e2614e99eb52b.js",
   "static/chunks/03cbea769ec72539.js",
-  "static/chunks/79cb3dcee67e04f1.css",
-  "static/chunks/turbopack-c216d71b70496618.js"
+  "static/chunks/2f16af747f816679.css",
+  "static/chunks/turbopack-1f85f57db2a94881.js"
 ])
