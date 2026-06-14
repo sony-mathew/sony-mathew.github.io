bf00e298033a03d82bb8a2f20585cfa66bd871ce
Sony Mathew <2040199+sony-mathew@users.noreply.github.com>
2026-06-14 22:31:48 +0530

    Add Base64 encoder decoder project

---
+++ _next/static/chunks/05d3wzjpq0d.s.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/projects/sip-calculator", [
+  "static/chunks/06xq5cli1.n23.js",
+  "static/chunks/06yrvkdymrq.y.js",
+  "static/chunks/0wuynw69z1s-x.js",
+  "static/chunks/0i0bw9-2mky53.js",
+  "static/chunks/0gjzdfwfq46th.css",
+  "static/chunks/turbopack-100xlaj22kwz3.js"
+])
---
+++ _next/static/chunks/0_pxzd0mbdb0n.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/projects/base64-encoder-decoder", [
+  "static/chunks/06xq5cli1.n23.js",
+  "static/chunks/0i0bw9-2mky53.js",
+  "static/chunks/0wuynw69z1s-x.js",
+  "static/chunks/0t5-x_ecxw8iu.js",
+  "static/chunks/0gjzdfwfq46th.css",
+  "static/chunks/turbopack-0ig02t.vxtld3.js"
+])
